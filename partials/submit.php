a607c6d9f8ac0f0c5026e802a39e9de495f5e19f
Ellis Sutehall <ellissutehall@gmail.com>
2019-10-27 15:26:41 +0000

    matches local mail config with remote

--- partials/submit.php
+++ partials/submit.php
@@ -13,11 +13,15 @@ $message = $_POST['message'];
 
 $formcontent = "From: $fname $lname \n $phone \n Message: $message";
 
+$headers = "From: $fname < $email >\n";
+
+$headers .= "Sender: <info@soulpower.biz> \n";
+
+// $recipient = "ellissutehall@gmail.com";
+
 $recipient = "dave145s@icloud.com";
 
-$mailheader = "From: $email \r\n";
-
-mail( $recipient, $subject, $formcontent ) or die ("Error!");
+mail( $recipient, $subject, $formcontent, $headers ) or die ("Error!");
 
 header( "refresh:1; url=https://www.soulpower.biz/thank-you.html" );
 
